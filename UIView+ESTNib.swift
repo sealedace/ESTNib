99d00925a570847a75fc08ff62eb87184b3197a0
sealedace <xgqpower@163.com>
2020-01-09 10:33:55 +0800

    Swift 5.0 compatibility

--- UIView+ESTNib.swift
+++ UIView+ESTNib.swift
@@ -9,20 +9,17 @@
 import UIKit
 
 extension UIView {
-    
-    public override func awakeAfterUsingCoder(aDecoder: NSCoder) -> AnyObject? {
-        
-        let nibName = String(Mirror(reflecting:self).subjectType)
-        
-        guard NSBundle.mainBundle().pathForResource(nibName, ofType: "nib") != nil else {
-            return super.awakeAfterUsingCoder(aDecoder)
+    open override func awakeAfter(using coder: NSCoder) -> Any? {
+        let nibName = String(reflecting: Mirror(reflecting: self).subjectType)
+
+        guard Bundle.main.path(forResource: nibName, ofType: "nib") != nil else {
+            return super.awakeAfter(using: coder)
         }
         
         if !translatesAutoresizingMaskIntoConstraints {
             //Prevents infinite loop from loadNibNamed internally-calling awakeAfterUsingCoder. Is false when called from storyboard, true when called from loadNibNamed.
-            
-            let replaced = NSBundle.mainBundle().loadNibNamed(nibName, owner: nil, options: nil).first! as! UIView
-            
+            let replaced = Bundle.main.loadNibNamed(nibName, owner: nil, options: nil)?.first as! UIView
+
             transferProperties(to: replaced)
             
             let newConstraints = reparentedConstraints(oldParent: self, newParent: replaced)
@@ -31,7 +28,7 @@ extension UIView {
             for view in subviews {
                 let subviewConstraint = view.reparentedConstraints(oldParent: self, newParent: replaced)
                 view.removeFromSuperview()
-                replaced.insertSubview(view, atIndex: replaced.subviews.endIndex)
+                replaced.insertSubview(view, at: replaced.subviews.endIndex)
                 view.addConstraints(subviewConstraint)
             }
             
@@ -42,12 +39,12 @@ extension UIView {
         return self
     }
     
-    private func transferProperties<T:UIView>(to to: T) {
+    private func transferProperties<T:UIView>(to: T) {
         to.translatesAutoresizingMaskIntoConstraints = false
         to.autoresizingMask = autoresizingMask
-        to.hidden = hidden
+        to.isHidden = self.isHidden
         to.tag = tag
-        to.userInteractionEnabled = userInteractionEnabled
+        to.isUserInteractionEnabled = self.isUserInteractionEnabled
         to.frame = frame
         to.bounds = bounds
         to.clipsToBounds = clipsToBounds
@@ -55,13 +52,13 @@ extension UIView {
         // Could use better reflection of properties!
     }
     
-    private func reparentedConstraints(oldParent oldParent:UIView, newParent:UIView) -> [NSLayoutConstraint]
+    private func reparentedConstraints(oldParent:UIView, newParent:UIView) -> [NSLayoutConstraint]
     {
         return constraints.map { constraint -> NSLayoutConstraint in
             let firstItem = oldParent == constraint.firstItem as? UIView ? newParent : constraint.firstItem
             let secondItem = oldParent == constraint.secondItem as? UIView ? newParent : constraint.secondItem
             let newConstraint = NSLayoutConstraint(
-                item: firstItem,
+                item: firstItem!,
                 attribute: constraint.firstAttribute,
                 relatedBy: constraint.relation,
                 toItem: secondItem,
@@ -72,7 +69,7 @@ extension UIView {
             newConstraint.priority = constraint.priority
             newConstraint.shouldBeArchived = constraint.shouldBeArchived
             newConstraint.identifier = constraint.identifier
-            newConstraint.active = constraint.active
+            newConstraint.isActive = constraint.isActive
             
             return newConstraint
         }
